354e74b78e96a13092e140f3ce51b9b0dad4699e
Maxim <chemaxa@gmail.com>
2018-06-05 18:15:42 +0300

    Added auth form markup

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,22 +1,93 @@
-import React from "react";
+import React, { Component } from "react";
 import fakeAuth from "../services/Auth";
-import { withRouter } from "react-router-dom";
-const AuthForm = withRouter(
-  ({ history }) =>
-    fakeAuth.isAuthenticated ? (
+import { withRouter, Redirect } from "react-router-dom";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormFeedback,
+  FormText,
+  Col,
+  Row
+} from "reactstrap";
+
+class AuthForm extends Component {
+  constructor(props) {
+    super(props);
+    this.from = this.props.location.state
+      ? this.props.location.state.from
+      : { from: { pathname: "/" } };
+    this.state = {
+      redirectToReferrer: false
+    };
+  }
+
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState({ redirectToReferrer: true });
+    });
+  };
+
+  renderForm() {
+    return (
+      <Row className="justify-content-center mt-1">
+        <Col md="6">
+          <p>You must log in to view the page at {this.from.pathname}</p>
+          <Form>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+              />
+              <FormFeedback>You will not be able to see this</FormFeedback>
+              <FormText>Example help text that remains unchanged.</FormText>
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                id="password"
+                valid
+              />
+              <FormFeedback valid>
+                You will not be able to see this
+              </FormFeedback>
+              <FormText>Example help text that remains unchanged.</FormText>
+            </FormGroup>
+            <Button onClick={this.login}>Submit</Button>
+          </Form>
+        </Col>
+      </Row>
+    );
+  }
+  render() {
+    const { redirectToReferrer } = this.state;
+
+    if (redirectToReferrer) {
+      return <Redirect to={this.from} />;
+    }
+
+    return fakeAuth.isAuthenticated ? (
       <p>
         Welcome!{" "}
         <button
           onClick={() => {
-            fakeAuth.signout(() => history.push("/"));
+            fakeAuth.signout(() => this.props.history.push("/"));
           }}
         >
           Sign out
         </button>
       </p>
     ) : (
-      <p>You are not logged in.</p>
-    )
-);
-
-export default AuthForm;
+      this.renderForm()
+    );
+  }
+}
+export default withRouter(AuthForm);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,6 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import fakeAuth from "../services/Auth";
+import React from "react";
 import AuthForm from "../components/AuthForm";
 
-export default class LoginPage extends Component {
-  state = {
-    redirectToReferrer: false
-  };
+const LoginPage = () => <AuthForm />;
 
-  login = () => {
-    fakeAuth.authenticate(() => {
-      this.setState({ redirectToReferrer: true });
-    });
-  };
-
-  render() {
-    const { from } = this.props.location.state || { from: { pathname: "/" } };
-    const { redirectToReferrer } = this.state;
-
-    if (redirectToReferrer) {
-      return <Redirect to={from} />;
-    }
-
-    return (
-      <div>
-        <p>You must log in to view the page at {from.pathname}</p>
-        <AuthForm />
-        <button onClick={this.login}>Log in</button>
-      </div>
-    );
-  }
-}
+export default LoginPage;
