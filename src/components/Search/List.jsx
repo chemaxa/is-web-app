0d98d0c9a4e1d7544b8d105b0cf600f89a8264a0
Maxim <chemaxa@gmail.com>
2018-06-06 15:59:25 +0300

    Removed logs

--- src/components/Search/List.jsx
+++ src/components/Search/List.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import SearchItem from "./Item";
 import data from "../../items.json";
-console.log(data.items);
 const SearchList = () => {
   return data.items.map(item => <SearchItem item={item} key={item.id} />);
 };
