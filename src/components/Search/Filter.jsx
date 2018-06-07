57636c909409ec153d8ee0a9d7af260781e2bc31
Maxim <chemaxa@gmail.com>
2018-06-07 11:15:42 +0300

    Refactoring

--- src/components/Search/Filter.jsx
+++ src/components/Search/Filter.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import DatePicker from "react-datepicker";
-import moment from "moment";
 import {
   CustomInput,
   Form,
@@ -8,90 +7,111 @@ import {
   Label,
   Input,
   Row,
-  Col
+  Col,
+  Button
 } from "reactstrap";
 
-export default class SearchFilter extends Component {
-  state = {
-    startDate: moment()
-  };
-  handleChange = date => {
-    this.setState({
-      startDate: date
-    });
-  };
-  render() {
-    return (
-      <Form className="mt-3">
-        <Row>
-          <Col md="4">
-            <FormGroup>
-              <Label>From</Label>
-              <DatePicker
-                className="form-control"
-                selected={this.state.startDate}
-                onChange={this.handleChange}
-              />
-            </FormGroup>
-          </Col>
-          <Col md="4">
-            <FormGroup>
-              <Label>To</Label>
-              <DatePicker
-                className="form-control"
-                selected={this.state.startDate}
-                onChange={this.handleChange}
-              />
-            </FormGroup>
-          </Col>
-          <Col md="4">
-            <FormGroup>
-              <Label for="inStock">In Stock only</Label>
-              <CustomInput type="checkbox" id="inStock" />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Row>
-          <Col md="4">
-            <FormGroup>
-              <Label for="price-from">Price from</Label>
-              <Input
-                type="number"
-                name="number"
-                id="price-from"
-                pattern="[0-9]+([\.,][0-9]{2})?"
-                step="0.01"
-                placeholder="Price from"
-              />
-            </FormGroup>
-          </Col>
-          <Col md="4">
-            <FormGroup>
-              <Label for="price-to">Price to</Label>
-              <Input
-                type="number"
-                name="number"
-                pattern="[0-9]+([\.,][0-9]{2})?"
-                step="0.01"
-                id="price-to"
-                placeholder="Price to"
-              />
-            </FormGroup>
-          </Col>
-          <Col md="4">
-            <FormGroup>
-              <Label for="color">Color</Label>
-              <Input type="select" name="color" id="color">
-                <option value="red">Red</option>
-                <option value="white">White</option>
-                <option value="black">Black</option>
-                <option value="yellow">Yellow</option>
-                <option value="green">Green</option>
-              </Input>
-            </FormGroup>
-          </Col>
-        </Row>
-      </Form>
-    );
-  }
-}
+const SearchFilter = ({ values, handlers }) => {
+  return (
+    <Form className="mt-3">
+      <Row>
+        <Col md="4">
+          <FormGroup>
+            <Label>From</Label>
+            <DatePicker
+              className="form-control"
+              selectsStart
+              selected={values.startDate}
+              startDate={values.startDate}
+              endDate={values.endDate}
+              onChange={handlers.handleChangeStartDate}
+            />
+          </FormGroup>
+        </Col>
+        <Col md="4">
+          <FormGroup>
+            <Label>To</Label>
+            <DatePicker
+              className="form-control"
+              selectsEnd
+              selected={values.endDate}
+              startDate={values.startDate}
+              endDate={values.endDate}
+              onChange={handlers.handleChangeEndDate}
+            />
+          </FormGroup>
+        </Col>
+        <Col md="4">
+          <FormGroup>
+            <Label for="inStock">In Stock only</Label>
+            <CustomInput
+              type="checkbox"
+              id="inStock"
+              name="inStock"
+              checked={values.inStock}
+              onChange={handlers.handleChange}
+            />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col md="4">
+          <FormGroup>
+            <Label for="price-from">Price from</Label>
+            <Input
+              type="number"
+              name="priceFrom"
+              value={values.priceFrom}
+              id="price-from"
+              pattern="[0-9]+([\.,][0-9]{2})?"
+              step="0.01"
+              placeholder="Price from"
+              onChange={handlers.handleChange}
+            />
+          </FormGroup>
+        </Col>
+        <Col md="4">
+          <FormGroup>
+            <Label for="price-to">Price to</Label>
+            <Input
+              type="number"
+              name="priceTo"
+              value={values.priceTo}
+              pattern="[0-9]+([\.,][0-9]{2})?"
+              step="0.01"
+              id="price-to"
+              placeholder="Price to"
+              onChange={handlers.handleChange}
+            />
+          </FormGroup>
+        </Col>
+        <Col md="4">
+          <FormGroup>
+            <Label for="color">Color</Label>
+            <Input
+              type="select"
+              name="color"
+              id="color"
+              value={values.color}
+              onChange={handlers.handleChange}
+            >
+              <option value="">Please choose color</option>
+              <option value="red">Red</option>
+              <option value="white">White</option>
+              <option value="black">Black</option>
+              <option value="yellow">Yellow</option>
+              <option value="green">Green</option>
+            </Input>
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row className="justify-content-end">
+        <Col md="1">
+          <Button onClick={handlers.clearFilter}>Clear</Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default SearchFilter;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,78 @@ import React, { Component } from "react";
 import SearchFilter from "../components/Search/Filter";
 import SearchList from "../components/Search/List";
 import data from "../resource/items.json";
+import moment from "moment";
 export default class SearchPage extends Component {
+  state = {
+    filters: {
+      startDate: moment(),
+      endDate: moment(),
+      priceFrom: "",
+      priceTo: "",
+      color: "",
+      inStock: false
+    }
+  };
+
+  handleChange = event => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    const { filters } = this.state;
+    this.setState({
+      filters: {
+        ...filters,
+        [name]: value
+      }
+    });
+  };
+
+  handleChangeStartDate = date => {
+    console.log(date.format());
+    const { filters } = this.state;
+    this.setState({
+      filters: {
+        ...filters,
+        startDate: date
+      }
+    });
+  };
+
+  handleChangeEndDate = date => {
+    console.log(date.format());
+    const { filters } = this.state;
+    this.setState({
+      filters: {
+        ...filters,
+        endDate: date
+      }
+    });
+  };
+
+  clearFilter = () => {
+    this.setState({
+      filters: {
+        startDate: moment(),
+        endDate: moment(),
+        priceFrom: "",
+        priceTo: "",
+        color: "",
+        inStock: false
+      }
+    });
+  };
   render() {
     return (
       <React.Fragment>
-        <SearchFilter />
+        <SearchFilter
+          handlers={{
+            handleChange: this.handleChange,
+            handleChangeStartDate: this.handleChangeStartDate,
+            handleChangeEndDate: this.handleChangeEndDate,
+            clearFilter: this.clearFilter
+          }}
+          values={{ ...this.state.filters }}
+        />
         <hr />
         <SearchList items={data.items} />
       </React.Fragment>
